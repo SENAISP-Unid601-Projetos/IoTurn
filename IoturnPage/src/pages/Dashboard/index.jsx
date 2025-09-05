05fec0a94dc3e7ea478b5eea9db0d8c6336c817d
MykyBalb <mykaelbalbino06@gmail.com>
2025-09-05 02:41:59 -0300

    Funcionalide gráfico contínuo implementada

--- IoturnPage/src/pages/Dashboard/index.jsx
+++ IoturnPage/src/pages/Dashboard/index.jsx
@@ -19,7 +19,7 @@ const MAX_DATA_POINTS = 60;
 
 const createChartOptions = (name, color) => ({
   chart: {
-    background: 'rgba(255, 255, 255, 0.2)',
+    background: "rgba(255, 255, 255, 0.2)",
     type: "area",
     height: 350,
     zoom: { enabled: false },
@@ -60,65 +60,12 @@ const createChartOptions = (name, color) => ({
   tooltip: { theme: "light", x: { format: "HH:mm:ss" } },
 });
 
-// const createGaugeOptions = (subtitle, pointRef) => ({
-//   debug: false,
-//   type: "gauge",
-//   chartArea: {
-//     background: { fill: "transparent", outline: { width: 0 } },
-//   },
-//   backgroundColor: "transparent",
-//   animation_duration: 1000,
-//   legend_visible: false,
-//   xAxis: { spacingPercentage: 0.25 },
-//   yAxis: {
-//     scale_range: [0, 100],
-//     visible: false,
-//     line: {
-//       width: 0,
-//       color: "smartPalette",
-//       breaks_gap: 0.06,
-//     },
-//   },
-//   palette: {
-//     pointValue: "{%value/100}",
-//     colors: ["#87CEFA", "#2d86e5", "#194869"],
-//   },
-//   defaultTooltip_enabled: false,
-//   defaultSeries: {
-//     angle: { sweep: 180 },
-//     shape: {
-//       innerSize: "70%",
-//       label: {
-//         text: `<span style="color:#333333; font-size:46px;">{%sum:n1}</span><br/><span style="color:#696969; font-size:20px;">${subtitle}</span>`,
-//         verticalAlign: "middle",
-//       },
-//     },
-//   },
-//   series: [
-//     {
-//       type: "column roundcaps",
-//       points: [{ id: "1", x: "speed", y: 0 }],
-//     },
-//   ],
-//   events: {
-//     load: function () {
-//       pointRef.current = this.series(0).points(0);
-//     },
-//   },
-// });
-
 const Dashboard = () => {
   const [machine] = useState(machineData);
   const [tempData, setTempData] = useState([]);
-  /* const [correntData, setCorrentData] = useState([]); */
   const chartTempRef = useRef(null);
-  /* const chartCorrentRef = useRef(null);
-  const gaugeRPMRef = useRef(null);
-  const gaugeNOleoRef = useRef(null); */
   const tempChart = useRef(null);
-  /* const correntChart = useRef(null);
-  const rpmPoint = useRef(null);
-  const nOleoPoint = useRef(null); */
+  const intervalIdRef = useRef(null);
 
   useEffect(() => {
     if (chartTempRef.current && !tempChart.current) {
@@ -126,91 +73,58 @@ const Dashboard = () => {
       tempChart.current = new ApexCharts(chartTempRef.current, optionsTemp);
       tempChart.current.render();
     }
-    /* if (chartCorrentRef.current && !correntChart.current) {
-      const optionsCorrent = createChartOptions("Corrente (A)", "#FF4560");
-      correntChart.current = new ApexCharts(
-        chartCorrentRef.current,
-        optionsCorrent
-      );
-      correntChart.current.render();
-    }
-    if (gaugeRPMRef.current) {
-      JSC.chart(gaugeRPMRef.current, createGaugeOptions("RPM", rpmPoint));
-    }
-    if (gaugeNOleoRef.current) {
-      JSC.chart(
-        gaugeNOleoRef.current,
-        createGaugeOptions("Nível Óleo", nOleoPoint)
 
-      );
-    } */
+    const restartInterval = () => {
+      clearInterval(intervalIdRef.current);
+      intervalIdRef.current = setInterval(() => {
+        setTempData((prevData) => {
+          if (prevData.length === 0) return prevData;
+          const lastPoint = prevData[prevData.length - 1];
+          const newPoint = { x: new Date().getTime(), y: lastPoint.y };
+          return [...prevData.slice(-MAX_DATA_POINTS + 1), newPoint];
+        });
+      }, 5000);
+    };
 
     // Conexão MQTT
-    const client = mqtt.connect("ws://10.42.0.1:9001");
+    //10.42.0.1:9001
+    const client = mqtt.connect("ws://test.mosquitto.org:8080");
     client.on("connect", () => {
       console.log("Conectado ao broker MQTT!");
       client.subscribe(`ioturn/temp`);
-      /* client.subscribe(`ioturn/corrente`);
-      client.subscribe(`ioturn/rpm`);
-      client.subscribe(`ioturn/nivelOleo`); */
     });
 
     client.on("message", (topic, message) => {
       try {
-        let value = message.toString();
-        let jsonDATA = JSON.parse(value);
-        jsonDATA = jsonDATA.temp;
-        const timestamp = new Date().getTime();
+        const value = message.toString();
+        const jsonDATA = JSON.parse(value);
+        const tempValue = Math.round(jsonDATA.temp);
 
-        if (topic === `ioturn/temp`) {
-          let tickAmountChecker = 5;
-          const tempValue = Math.round(jsonDATA);
-          if (tempValue == -127) return
-
-          if (tempValue > tickAmountChecker) {
-            tickAmountChecker = tempValue / 2;
-            if (tempChart.current) {
-              tempChart.current.updateOptions({
-                yaxis: {
-                  tickAmount: tickAmountChecker,
-                },
-              });
-            }
-          }
-
-          setTempData((prev) => [
-            ...prev.slice(-MAX_DATA_POINTS + 1),
-            { x: timestamp, y: tempValue },
+        if (tempValue !== -127) {
+          const newPoint = {
+            x: new Date().getTime(),
+            y: tempValue,
+          };
+          setTempData((prevData) => [
+            ...prevData.slice(-MAX_DATA_POINTS + 1),
+            newPoint,
           ]);
-        } /* else if (topic === `ioturn/corrente`) {
-          setCorrentData((prev) => [
-            ...prev.slice(-MAX_DATA_POINTS + 1),
-            { x: timestamp, y: value },
-          ]);
-        } else if (topic === `ioturn/rpm`) {
-          if (rpmPoint.current) rpmPoint.current.options({ y: value });
-        } else if (topic === `ioturn/nivelOleo`) {
-          if (nOleoPoint.current) nOleoPoint.current.options({ y: value });
-        } */
+          restartInterval();
+        }
       } catch (e) {
         console.error("Erro ao processar mensagem MQTT:", e);
       }
     });
 
+    restartInterval();
+
     return () => {
-      if (client) {
-        client.end();
-      }
+      clearInterval(intervalIdRef.current);
+      if (client) client.end();
       if (tempChart.current) {
         tempChart.current.destroy();
         tempChart.current = null;
       }
-      /* if (correntChart.current) {
-        correntChart.current.destroy();
-        correntChart.current = null;
-      }
-      if (gaugeRPMRef.current) gaugeRPMRef.current.innerHTML = "";
-      if (gaugeNOleoRef.current) gaugeNOleoRef.current.innerHTML = ""; */
     };
   }, []);
 
@@ -219,12 +133,6 @@ const Dashboard = () => {
       tempChart.current.updateSeries([{ data: tempData }]);
     }
   }, [tempData]);
-
-  /* useEffect(() => {
-    if (correntChart.current && correntData.length > 0) {
-      correntChart.current.updateSeries([{ data: correntData }]);
-    }
-  }, [correntData]); */
 
   return (
     <div className="flex bg-[#1a2a3a] text-white min-h-screen">
@@ -239,53 +147,29 @@ const Dashboard = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div>
                 <p className="text-sm text-slate-600">Nome:</p>
-                <p className="text-lg font-medium">
-                  {machine.name}
-                </p>
+                <p className="text-lg font-medium">{machine.name}</p>
               </div>
               <div>
                 <p className="text-sm text-slate-600">Modelo:</p>
-                <p className="text-lg font-medium">
-                  {machine.model}
-                </p>
+                <p className="text-lg font-medium">{machine.model}</p>
               </div>
               <div>
                 <p className="text-sm text-slate-600">Fabricante:</p>
-                <p className="text-lg font-medium">
-                  {machine.fabricante}
-                </p>
+                <p className="text-lg font-medium">{machine.fabricante}</p>
               </div>
               <div>
                 <p className="text-sm text-slate-600">Numeração:</p>
-                <p className="text-lg font-medium">
-                  {machine.numeracao}
-                </p>
+                <p className="text-lg font-medium">{machine.numeracao}</p>
               </div>
             </div>
           </div>
           <div className="flex flex-col lg:flex-row flex-wrap gap-6 mb-6">
-            {/* <div className="bg-white w-full rounded-2xl p-6 shadow-lg lg:flex-1">
-              <h3 className="text-xl font-semibold mb-4 text-slate-800">
-                Corrente
-              </h3>
-              <div ref={chartCorrentRef} className="echart-box"></div>
-            </div> */}
-            {/* <div className="bg-white w-full rounded-2xl p-6 shadow-lg lg:flex-1 flex flex-col justify-center">
-              <h3 className="text-xl font-semibold mb-4 text-slate-800">RPM</h3>
-              <div ref={gaugeRPMRef} className="gauge-container"></div>
-            </div> */}
             <div className="bg-white w-full rounded-2xl p-6 shadow-lg lg:flex-1">
               <h3 className="text-xl font-semibold mb-4 text-slate-800">
                 Temperatura
               </h3>
               <div ref={chartTempRef} className="echart-box"></div>
             </div>
-            {/* <div className="bg-white w-full rounded-2xl p-6 shadow-lg lg:flex-1 flex flex-col justify-center">
-              <h3 className="text-xl font-semibold mb-4 text-slate-800">
-                Nível de Óleo
-              </h3>
-              <div ref={gaugeNOleoRef} className="gauge-container"></div>
-            </div> */}
           </div>
         </main>
       </div>

da310f45ebc7065d220a7b8245254ae2129d0874
Pedro Leme <151878192+PhrL0@users.noreply.github.com>
2025-09-15 15:04:33 -0300

    Update MVP_SENDER.ino

--- MVP_SENDER/MVP_SENDER.ino
+++ MVP_SENDER/MVP_SENDER.ino
@@ -12,13 +12,13 @@
 
 #define RF_FREQUENCY                                915000000 // Hz
 
-#define TX_OUTPUT_POWER                             5        // dBm
+#define TX_OUTPUT_POWER                             20        // dBm, PADRÃO FUNCIONAL: 5
 
 #define LORA_BANDWIDTH                              0         // [0: 125 kHz,
                                                               //  1: 250 kHz,
                                                               //  2: 500 kHz,
                                                               //  3: Reserved]
-#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_SPREADING_FACTOR                       9         // [SF7..SF12] PADRÃO FUNCIONAL: 7
 #define LORA_CODINGRATE                             1         // [1: 4/5,
                                                               //  2: 4/6,
                                                               //  3: 4/7,

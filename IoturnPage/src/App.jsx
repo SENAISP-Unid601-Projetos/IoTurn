cc20ba84e2f720c86a49b4ff210f56f698fef227
MykyBalb <mykaelbalbino06@gmail.com>
2025-09-24 11:22:14 -0300

    Reolving Merge Problems

--- IoturnPage/src/App.jsx
+++ IoturnPage/src/App.jsx
@@ -2,19 +2,14 @@ import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
-import MachineSelection from "./pages/MachineSelection";
 import Chat from "./pages/Chat";
-import LoginPage from "./pages/LoginPage"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/selecao" element={<MachineSelection />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/chatbot" element={<Chat />} /> 
-      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }

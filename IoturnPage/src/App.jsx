be484d6f0c73ce56a5ca89fcb2d1ac24a232fce2
MykyBalb <mykaelbalbino06@gmail.com>
2025-10-03 16:13:25 -0300

    Fix App.jsx

--- IoturnPage/src/App.jsx
+++ IoturnPage/src/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
-import LandingPage from "./pages/LandingPage";
-import Dashboard from "./pages/Dashboard";
 import Chat from "./pages/Chat";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/chatbot" element={<Chat />} /> 
+      <Route path="/" element={<Chat />} /> 
     </Routes>
   );
 }

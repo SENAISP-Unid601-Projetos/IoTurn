f5d6a25383cd032aa287335da39a30e3940ffe83
JonPer3z <perez.joaopedro@gmail.com>
2025-10-05 17:30:51 -0300

    Import_CssBaseline

--- IoturnPage/src/main.jsx
+++ IoturnPage/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 import App from "./App.jsx";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<BrowserRouter>
 			<ThemeProvider theme={theme}>
+				<CssBaseline />
 				<App />
 			</ThemeProvider>
 		</BrowserRouter>

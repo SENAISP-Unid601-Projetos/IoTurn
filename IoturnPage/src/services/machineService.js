3c8d7d1e1819083814994ed2052de39a9ef3af6b
perez.joaopedro@gmail.com <mykaelbalbino06@gmail.com>
2025-10-10 17:22:49 -0300

    Grid

--- IoturnPage/src/services/machineService.js
+++ IoturnPage/src/services/machineService.js
@@ -1,3 +1,46 @@
+// const API_URL = 'http://10.110.12.12:3000/machines/getAllUsersMachine/1';
+// const formatTimestamp = (timestamp) => {
+//   if (!timestamp) return 'N/A';
+//   const date = new Date(timestamp);
+//   return date.toLocaleString('pt-BR');
+// };
+
+
+// export const fetchAllMachineData = async () => {
+//   console.log("Buscando dados da nova API...");
+//   const response = await fetch(API_URL);
+
+//   if (!response.ok) {
+//     throw new Error('Falha ao buscar os dados da nova API');
+//   }
+
+//   const rawData = await response.json();
+//   console.log("Dados brutos recebidos:", rawData);
+
+//   const formattedData = rawData.map(machine => {
+//     return {
+//       id: machine.id,
+//       name: machine.name,
+//       sector: machine.model || '', 
+//       machineId: machine.serialNumber, 
+//       company: machine.manufacturer,   
+//       lastUpdate: formatTimestamp(machine.lastRpm?.timestamp), 
+
+//       metrics: [
+//         { name: 'RPM', value: machine.lastRpm?.rpm ?? 'N/A', unit: '' },
+//         { name: 'Temp', value: machine.lastOilTemperature?.temperature ?? 'N/A', unit: '°C' },
+//         { name: 'Óleo', value: machine.lastOilLevel?.level ?? 'N/A', unit: '%' },
+//         { name: 'Corrente', value: machine.lastCurrent?.current ?? 'N/A', unit: 'A' },
+//       ]
+//     };
+//   });
+
+//   console.log("Dados formatados para o front-end:", formattedData);
+//   return formattedData;
+// };
+
+
+
 const MACHINES_URL = 'http://localhost:3001/machines';
 const METRICS_BASE_URL = 'http://localhost:3001/metrics';
 

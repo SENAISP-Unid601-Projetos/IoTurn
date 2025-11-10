09c0068e2128b4b5bfbf8a6c56b5f045a726029c
MykyBalb <mykaelbalbino06@gmail.com>
2025-11-10 16:46:59 -0300

    removido duplicatas

--- IoturnPage/src/services/ApiServices.js
+++ IoturnPage/src/services/ApiServices.js
@@ -6,14 +6,13 @@ class ApiService {
   // Método GET
   static async getRequest(APIendpoint) {
     const fullUrl = `${API_BASE_URL}${APIendpoint}`;
+    console.log(fullUrl);
+
     try {
       const response = await axios.get(fullUrl);
       return response.data;
     } catch (error) {
-      console.error(
-        `Erro ao enviar requisição GET para ${fullUrl}: `,
-        error
-      );
+      console.error(`Erro ao enviar requisição GET para ${fullUrl}: `, error);
       throw error;
     }
   }
@@ -21,14 +20,13 @@ class ApiService {
   // Método POST
   static async postRequest(APIendpoint, data) {
     const fullUrl = `${API_BASE_URL}${APIendpoint}`;
+    console.log(fullUrl);
+
     try {
       const response = await axios.post(fullUrl, data);
       return response.data;
     } catch (error) {
-      console.error(
-        `Erro ao enviar a requisição POST para ${fullUrl}:`,
-        error
-      );
+      console.error(`Erro ao enviar a requisição POST para ${fullUrl}:`, error);
       throw error;
     }
   }

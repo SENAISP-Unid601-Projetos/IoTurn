da0ad73fb5fddb6d0d4a7f21f422dcb0b8ff32dd
MykyBalb <mykaelbalbino06@gmail.com>
2025-11-19 14:58:54 -0300

    Fix bugs re-render

--- IoturnPage/src/main.jsx
+++ IoturnPage/src/main.jsx
@@ -8,12 +8,10 @@ import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
   <BrowserRouter>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
     </ThemeProvider>
   </BrowserRouter>
-  // </StrictMode>
 );
